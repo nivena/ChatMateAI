8b1c4807e326fcd0beb898fff97ba29a527dec01
nivena <136405690+nivena@users.noreply.github.com>
2023-08-18 10:08:01 +1000

    Improve variable clarity with comments

--- whatsapp.go
+++ whatsapp.go
@@ -35,13 +35,13 @@ import (
 
 var cli *whatsmeow.Client
 var Log waLog.Logger
-
-var logLevel = "INFO"
-var debugLogs = flag.Bool("debug", false, "Enable debug logs?")
-var dbDialect = flag.String("db-dialect", "sqlite3", "Database dialect (sqlite3 or postgres)")
-var dbAddress = flag.String("db-address", "file:mdtest.db?_foreign_keys=on", "Database address")
-var requestFullSync = flag.Bool("request-full-sync", false, "Request full (1 year) history sync when logging in?")
-var pairRejectChan = make(chan bool, 1)
+// Additional insight for the variables below
+var logLevel = "INFO" // Set the log level
+var debugLogs = flag.Bool("debug", false, "Enable debug logs?") // Enable debug logs
+var dbDialect = flag.String("db-dialect", "sqlite3", "Database dialect (sqlite3 or postgres)") // Specify the database dialect
+var dbAddress = flag.String("db-address", "file:mdtest.db?_foreign_keys=on", "Database address") // Set the database address
+var requestFullSync = flag.Bool("request-full-sync", false, "Request full (1 year) history sync when logging in?") // Specify full history sync
+var pairRejectChan = make(chan bool, 1) // Channel for pair rejection
 
 func whatsappStart() {
     waBinary.IndentXML = true
